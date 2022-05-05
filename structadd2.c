1eeb18d244a8434c529e95b722f4719b4c3d69f2
shubhankar12 <104965588+Shubhamkar12@users.noreply.github.com>
2022-05-05 23:45:53 +0530

    Add files via upload

---
+++ structadd2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+struct distance
+{
+    int dis1;
+    int dis2;
+}d1={20},d2={30};
+int main()
+{
+    int temp;
+    temp=(d1+d2);
+    printf("\n SUM OF D1 AND D2 IS %d",structdistance.temp);
+    return 0;
+}
